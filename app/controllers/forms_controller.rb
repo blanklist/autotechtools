54c10bdbdc0dfb36ca922732d644b3295593a7e4
blanklist <joehineriv@gmail.com>
2017-08-10 11:14:42 -0700

    working on forms

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,2 +1,18 @@
 class FormsController < ApplicationController
+
+  def index
+    @user = current_user
+    @forms = Forms.all # where user is logged in
+  end
+
+  def new
+    @form = Forms.new
+  end
+
+  def create
+
+  end
+
+
+
 end
